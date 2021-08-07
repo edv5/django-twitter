cf93e715fe9ec59c9d959ec348aa9f40102c9940
Antong Su <georgesat19@gmail.com>
2021-08-06 22:43:25 -0700

    add logout api

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -4,15 +4,15 @@ from rest_framework import viewsets
 from rest_framework import permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from accounts.api.serializers import UserSerializer
+from accounts.api.serializers import UserSerializer, LoginSerializer
 from rest_framework.permissions import AllowAny
+
+#from accounts.api.serializers import SignupSerializer, LoginSerializer
 from django.contrib.auth import (
-    authenticate as django_authenticate,
     login as django_login,
     logout as django_logout,
+    authenticate as django_authenticate,
 )
-#from accounts.api.serializers import SignupSerializer, LoginSerializer
-from django.contrib.auth import logout as django_logout
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -23,6 +23,8 @@ class UserViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
 class AccountViewSet(viewsets.ViewSet):
+    serializer_class = LoginSerializer
+
     @action(methods=['GET'], detail=False)
     def login_status(self, request):
         data = {'has_logged_in': request.user.is_authenticated}
@@ -34,3 +36,33 @@ class AccountViewSet(viewsets.ViewSet):
     def logout(self, request):
         django_logout(request)
         return Response({'success': True})
+
+    @action(methods=['POST'], detail=False)
+    def login(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response({
+                "success": False,
+                "message": "Please check input",
+                "errors": serializer.errors,
+            }, status=400)
+        username = serializer.validated_data['username']
+        password = serializer.validated_data['password']
+
+        if not User.objects.filter(username=username).exists():
+            return Response({
+                "success": False,
+                "message": "Username does not exist",
+            }, status=400)
+
+        user = django_authenticate(username=username, password=password)
+        if not user or user.is_anonymous:
+            return Response({
+                "success": False,
+                "message": "Username and password does not match",
+            }, status=400)
+        django_login(request, user)
+        return Response({
+            "success": True,
+            "user": UserSerializer(instance=user).data,
+        })
